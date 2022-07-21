c515270763602d296ceafedac9268816d7c88eb5
Ananev-Alexandr <ananevalexandr@mail.ru>
2022-07-21 12:54:46 +0300

    new programm of Various combinations

---
+++ func_and_stack_call/various_combinations.py
@@ -0,0 +1,12 @@
+fist_value, second_value = map(int, input().split())
+
+
+def various_combinations(fist_value, second_value):
+    if second_value == 0:
+        return 1
+    elif second_value > fist_value:
+        return 0
+    else:
+        return various_combinations(fist_value - 1, second_value) + various_combinations(fist_value - 1, second_value - 1)
+    
+print(various_combinations(fist_value,second_value))
