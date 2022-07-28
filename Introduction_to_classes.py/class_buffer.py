93cad03c1ae7e61a49d32c8d689ff104c8b90362
Ananev-Alexandr <ananevalexandr@mail.ru>
2022-07-28 10:23:59 +0300

    [NT] #Task changed according to flake8

--- Introduction_to_classes.py/class_buffer.py
+++ Introduction_to_classes.py/class_buffer.py
@@ -1,7 +1,8 @@
 class Buffer:
     def __init__(self):
         self.buf = []
-    def add(self, *list_new_num): 
+
+    def add(self, *list_new_num):
             for num in list_new_num:
                 if len(self.buf) <= 4:
                     self.buf.append(num)
@@ -11,6 +12,7 @@ class Buffer:
                 else:
                     print(sum(self.buf[:5]))
                     del self.buf[:5]
-                    self.buf.append(num)        
+                    self.buf.append(num)
+
     def get_current_part(self):
         return self.buf
